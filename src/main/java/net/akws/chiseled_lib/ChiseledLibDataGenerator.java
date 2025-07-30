33442b6a8e9fc3bd417e7ebe54d9b2bdeb1a5fcb
InfinityFarzad <farzad.karami@outlook.com>
2025-07-30 11:22:33 -0700

    ChiseledLib 1.0.0

---
+++ src/main/java/net/akws/chiseled_lib/ChiseledLibDataGenerator.java
@@ -0,0 +1,11 @@
+package net.akws.chiseled_lib;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChiseledLibDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
