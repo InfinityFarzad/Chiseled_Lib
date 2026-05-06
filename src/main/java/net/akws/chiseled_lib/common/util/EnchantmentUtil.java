baaf4a798695f821b4258f01d33283c913455375
InfinityFarzad <10farzad.karami@gmail.com>
2026-05-06 16:59:16 +0330

    Rewrote enchantmentUtil
    im not sure if this works due to current restrictions, if someone could test this code and report if it works id appreciate it

--- src/main/java/net/akws/chiseled_lib/common/util/EnchantmentUtil.java
+++ src/main/java/net/akws/chiseled_lib/common/util/EnchantmentUtil.java
@@ -8,15 +8,15 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.entry.RegistryEntry;
 
 public class EnchantmentUtil {
+
     public static boolean hasEnchantment(ItemStack stack, RegistryKey<Enchantment> enchantment) {
-        return stack.getEnchantments().getEnchantments().toString().
-                contains(enchantment.getValue().toString());
+        return enchantment.getValue() != null & stack.getEnchantments().getEnchantments().toString().contains(enchantment.getValue().toString());
     }
 
-    public static int getLevel(ItemStack stack, RegistryKey<Enchantment> enchantmentRegistryKey) {
-        for (RegistryEntry<Enchantment> enchantments : stack.getEnchantments().getEnchantments()) {
-            if (enchantments.toString().contains(enchantmentRegistryKey.getValue().toString())) {
-                return stack.getEnchantments().getLevel(enchantments);
+    public static int getEnchantmentLevel(ItemStack stack, RegistryKey<Enchantment> enchantment) {
+        for(RegistryEntry<Enchantment> enchantEntry : stack.getEnchantments().getEnchantments()) {
+            if(enchantEntry.getKey().isPresent() && enchantEntry.getKey().get().equals(enchantment)) {
+                return stack.getEnchantments().getLevel(enchantEntry);
             }
         }
         return 0;
@@ -30,5 +30,4 @@ public class EnchantmentUtil {
             return ItemStack.EMPTY;
         }
     }
-
 }
